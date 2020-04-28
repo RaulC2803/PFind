75f4f45848508cb3e1b4106c1e7a2af0b423b6f7
Helsac <jonathanvillegas202000@gmail.com>
2020-04-28 01:20:44 -0500

    Add files via upload

---
+++ pfind/src/main/java/com/fcorp/pfind/ServiciosRest/ProductoRest.java
@@ -0,0 +1,43 @@
+package com.fcorp.pfind.ServiciosRest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.fcorp.pfind.Repositorio.Productorepositorio;
+import com.fcorp.pfind.Servicios.ServicioProducto;
+import com.fcorp.pfind.entity.Producto;
+
+@RestController
+@RequestMapping("/producto")
+public class ProductoRest {
+    @Autowired
+    private ServicioProducto servicioProducto;
+    
+    @GetMapping("/buscar/{pid}")
+    public Producto buscarProducto(@PathVariable(value = "pid") Long pid) {
+        try {
+            return servicioProducto.obtenerProducto(pid);
+        } catch(Exception e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Producto no encontrado");
+        }
+    }
+    
+    @PostMapping("/registrar")
+    public Producto registrarProducto(@RequestBody Producto p) {
+        try {
+            return servicioProducto.registrarProducto(p);
+        } catch(Exception e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
+        }
+    }
+    
+
+    
+}
