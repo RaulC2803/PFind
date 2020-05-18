f889ec5ca7a172f48b7da10dffee1a42b483f007
oscarburga <51001981+oscarburga@users.noreply.github.com>
2020-05-17 23:57:14 -0500

    Update Productorepositorio.java

--- pfind/src/main/java/com/fcorp/pfind/Repositorio/Productorepositorio.java
+++ pfind/src/main/java/com/fcorp/pfind/Repositorio/Productorepositorio.java
@@ -4,4 +4,7 @@ import com.fcorp.pfind.entity.Producto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface Productorepositorio extends JpaRepository<Producto, Long> {
+	@Query("select p from Producto p where p.nombre like %?1%")
+	public List<Producto> buscarPorNombre(String nombre);
 }
+
